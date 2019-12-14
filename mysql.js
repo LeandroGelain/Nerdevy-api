1dcd9ebf09713f3f3bb9e1d264939c70556a75ed
LeandroGelain03 <leandrogelain08@outlook.com>
2019-12-14 17:51:22 -0300

    Alterações api

--- mysql.js
+++ mysql.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql')
+const dotenv = require('dotenv')
+dotenv.config()
 
 var pool = mysql.createPool({
-    "user":"root",
-    "password":"root",
-    "database":"Education_project",
-    "host":"localhost",
+    "user":process.env.USER,
+    "password":process.env.PASSWORD,
+    "database":process.env.DATABASE,
+    "host":process.env.HOST,
     "port":3306
 });
 
